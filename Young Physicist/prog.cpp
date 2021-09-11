7cd2e68b412ec2f7a739616b566542745fd2d22b
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-09-11 12:30:32 +0530

    Create prog.cpp

---
+++ Young Physicist/prog.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+int32_t main(void)
+{
+    int n,finalsum=0,sumx=0,sumy=0,sumz=0;
+    cin>>n;
+    while(n--)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+        sumx=sumx+x;
+        sumy=sumy+y;
+        sumz=sumz+z;
+    }
+    if(sumx==0&&sumy==0&&sumz==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}

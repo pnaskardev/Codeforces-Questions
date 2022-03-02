322756d65895020e2807f14f1864b7bdb24ceff6
pnaskardev <pnaskardev@gmail.com>
2022-03-02 11:38:23 +0530

    main

--- B. Gregor and the Pawn Game/prog.cpp
+++ B. Gregor and the Pawn Game/prog.cpp
@@ -14,5 +14,41 @@ int32_t main(void)
 }
 void solve()
 {
-    
+    int n;
+    cin>>n;
+    string s1,s2;
+    cin>>s1>>s2;
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        if(s2[i]=='1')
+        {
+            if(s1[i]=='1')
+            {
+                int p=i-1,q=i+1;
+                if(p>=0 &&s1[p]!='2'&& s1[p]=='1')
+                {
+                    s1[p]='2';
+                    s2[i]='0';
+                    count++;
+                }
+                else if(q<n && s1[q]!='2' && s1[q]=='1')
+                {
+                    s1[q]='2';
+                    s2[i]='0';
+                    count++;
+                }
+            }
+            else 
+            {
+                s1[i]='2';
+                s1[i]='0';
+                count++;
+            }
+        }
+    }
+    cout<<count<<endl;
+    // cout<<s1<<endl;
+    // cout<<s2<<endl;
+    // cout<<endl;
 }
